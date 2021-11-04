1d1cbe9079bec5253b0a72a2db4eef56e84cdb42
CP050 <cosimopoccianti@gmail.com>
2021-11-04 14:40:49 +0100

    modifica API

--- weathercode.py
+++ weathercode.py
@@ -13,8 +13,10 @@ Latitude = ['41.8955','64.1353','31.7857','41.3949','38.9429','43.7683','43.6683
 Longitude = ['12.4823','-21.8952','35.2007','2.1756','16.3316','11.2590','12.5224']
 Location = ['Rome','Reykjavik','Jerusalem','Barcelona','Lamezia Terme','Florence','Urbania']
 
+
+
 for i in range(len(Location)):
-    url = 'https://api.open-meteo.com/v1/forecast?latitude='+ Latitude[i] +'&longitude='+ Longitude[i] +'&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&timezone=Europe%2FBerlin'
+    url = 'https://api.open-meteo.com/v1/forecast?latitude='+ Latitude[i] +'&longitude='+ Longitude[i] +'&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,pressure_msl,precipitation,freezinglevel_height&timezone=Europe%2FBerlin'
     response = requests.get(url)
     file = open("Forecast_"+ Location[i] +".json", "w+")
     print(file.name)
