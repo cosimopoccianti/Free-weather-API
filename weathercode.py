b02b7aa98aebe5efd980db77b60b4cdd868b6868
CP050 <cosimopoccianti@gmail.com>
2021-11-04 14:34:49 +0100

    Aggiunta la parte relativa al dataframe

--- weathercode.py
+++ weathercode.py
@@ -5,7 +5,8 @@ Created on Thu Nov  4 14:27:02 2021
 @author: marti
 """
 
-
+import pandas
+import json
 import requests
 
 Latitude = ['41.8955','64.1353','31.7857','41.3949','38.9429','43.7683','43.6683']
@@ -20,3 +21,25 @@ for i in range(len(Location)):
     file.writelines(response.text)
     file.close()
     
+result = []
+
+for city in Location:
+
+    json_data = json.load(open('Forecast_'+ city +'.json'))
+    
+    hourly = json_data['hourly']
+    
+    time = hourly['time'] 
+    temp = hourly['temperature_2m']
+    apptemp = hourly['apparent_temperature']
+    hum = hourly['relativehumidity_2m']
+    pre = hourly['precipitation']
+    
+    for i in range(len(time)):
+        x = [city] + [time[i]] + [temp[i]] + [apptemp[i]] + [hum[i]] + [pre[i]]
+        result.append(x)
+    
+csv_file_path = 'Forecast_Rome.csv'
+df = pandas.DataFrame(result)
+df.columns = ['city','time','temperature','apparent temperature','relative humidity','precipitation']
+df.to_csv(csv_file_path, index=False)
